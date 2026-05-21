b5014cda38b019ba39170bf59c1e21669a7444fb
tharnika <tharnikareddy01@gmail.com>
2026-05-22 00:33:30 +0530

    initial commit

--- models/marts/dim_products.sql
+++ models/marts/dim_products.sql
@@ -1,10 +1,9 @@
--- Final clean product list for reporting
 select
     product_id,
     product_name,
     category,
     unit_price,
     cost_price,
-    unit_price - cost_price            as gross_margin,   -- profit per unit
+    unit_price - cost_price            as gross_margin,  
     in_stock
 from {{ ref('stg_products') }}
